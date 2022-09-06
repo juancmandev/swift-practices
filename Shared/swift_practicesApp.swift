5f9391d08a6a7f925ff8a90d15a2aaffaf7332fc
Juan Carlos Manzanero Domínguez <whenix.developer@gmail.com>
2022-09-05 20:26:53 -0500

    create swift project

---
+++ Shared/swift_practicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practicesApp.swift
+//  Shared
+//
+//  Created by Juan Carlos Manzanero Domínguez on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_practicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
